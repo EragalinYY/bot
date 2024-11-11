89fd243e1a9e01bc5df456ef197d7b9f796b4017
Yury Eragalin <EragalinYY@gmail.com>
2024-11-11 21:02:45 +0400

    Стартуем

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func main() {
+	conn, _ := net.Dial("tcp", "golang.org:80")         //Соединение по TCP
+	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")         //Отправка строки через
+	status, _ := bufio.NewReader(conn).ReadString('\n') //Вывод первой строки ответа
+	fmt.Println(status)
+}
